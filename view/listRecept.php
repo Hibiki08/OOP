b3b2c1cfdd7ae3adb200441950bf6198dbf8b7b6
Hibiki08 <devilcatt1@gmail.com>
2015-02-21 23:00:25 +0300

    Выведен и упорядочен список рецептов

--- view/listRecept.php
+++ view/listRecept.php
@@ -1,1 +1,24 @@
 <?php
+include_once '..\classes\BD_otladchik.php';
+?>
+
+<html>
+    <head>
+        <meta charset="utf-8" />
+    </head>
+    <body>
+    <?php
+    $array = new SQL_work;
+    $selectAll_array = $array->SelectAll_array();
+
+    foreach ($selectAll_array as $values) { ?>
+
+    <ul>
+        <li><?php echo $values['Name']; ?></li>
+    </ul>
+
+    <?php
+    }
+    ?>
+    </body>
+</html>
