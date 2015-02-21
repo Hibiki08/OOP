fb5539bf9a503be2fefecdb4e6f805cd560c8eeb
Hibiki08 <devilcatt1@gmail.com>
2015-02-21 20:44:12 +0300

    Класс рецептов

--- Classes/recept.php
+++ Classes/recept.php
@@ -1,8 +1,15 @@
 <?php
-abstract class Recept {
+abstract class abstractRecept {
     public $name;
     public $ingredients;
     public $cook;
     abstract public function Recept($name, $ingredients, $cook);
 }
 
+class Recept extends abstractRecept {
+    public function Recept($name, $ingredients, $cook) {
+        echo $this->name = $name;
+        echo $this->ingredients = $ingredients;
+        echo $this->cook = $cook;
+    }
+}
