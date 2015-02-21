3dd2375942f7dc0ecf774833149540756e38f1a1
Hibiki08 <devilcatt1@gmail.com>
2015-02-21 18:16:21 +0300

    Добавлен класс для работы с БД

--- Classes/SQL.php
+++ Classes/SQL.php
@@ -1,7 +1,26 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: Hibiki
- * Date: 21.02.2015
- * Time: 15:22
- */ 
+class SQL_work {
+    public $name;
+    public $ingredients;
+    public $cook;
+    public $cell;
+    public $value;
+    public $id;
+    public $id_number;
+    public function __construct() {
+        mysql_connect('localhost', 'root', '');
+        mysql_select_db('ШП');
+    }
+    public function Insert($name, $ingredients, $cook) {
+        mysql_query("INSERT INTO News (Name, Ingredients, Cook) VALUES (" . "'" . $this->name = $name . "'" . "," .
+        "'" . $this->ingredients = $ingredients . "'" . "," . "'" . $this->cook = $cook . "'" . ")");
+    }
+    public function Update($cell, $value, $id, $id_number) {
+        mysql_query("UPDATE News SET" . " " . $this->cell = $cell . "=" . "'" . $this->value = $value . "'" . " " .
+        "WHERE" . " " . $this->id = $id . "=" . "'" . $this->id_number = $id_number . "'");
+    }
+    public function Select() {
+        mysql_query("SELECT * FROM News");
+    }
+}
+
