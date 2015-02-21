0b96b7dd56f9bbfe07b919feab6a471f524ba183
Hibiki08 <devilcatt1@gmail.com>
2015-02-21 20:59:30 +0300

    Разметка формы

---
+++ view/form.php
@@ -0,0 +1,18 @@
+<html>
+    <head>
+        <meta charset="utf-8"/>
+    </head>
+    <body>
+        <form action="" method="post">
+            <label>Название:
+            <input type="text" name="name"/>
+            </label>
+            <label>Ингредиенты:
+            <textarea name="ingredients"></textarea>
+            </label>
+            <label>Приготовление:
+            <textarea name="cook"></textarea>
+            </label>
+        </form>
+    </body>
+</html>
