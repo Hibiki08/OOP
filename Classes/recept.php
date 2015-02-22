5c46df68e948fc9ce400d68b2638f077595fe9ae
Hibiki08 <devilcatt1@gmail.com>
2015-02-22 15:16:28 +0300

    Класс отображения рецепта

--- Classes/recept.php
+++ Classes/recept.php
@@ -3,13 +3,13 @@ abstract class abstractRecept {
     public $name;
     public $ingredients;
     public $cook;
-    abstract public function Recept($name, $ingredients, $cook);
+    abstract public function Recept();
 }
 
 class Recept extends abstractRecept {
-    public function Recept($name, $ingredients, $cook) {
-        echo $this->name = $name;
-        echo $this->ingredients = $ingredients;
-        echo $this->cook = $cook;
+    public function Recept() {
+        echo $this->name;
+        echo $this->ingredients;
+        echo $this->cook;
     }
 }
