9f30a8a95fb8d7e373832082997b6c2d09141bec
Hibiki08 <devilcatt1@gmail.com>
2015-02-22 15:41:11 +0300

    Добавлен обработчик формы

--- model/new_recept.php
+++ model/new_recept.php
@@ -1,1 +1,22 @@
 <?php
+include_once '..\classes\SQL.php';
+
+if (isset($_POST) && (empty($_POST['name']) ||
+                    empty($_POST['ingredients']) ||
+                    empty($_POST['cook']))) { ?>
+
+<p>Не все поля заполнены!</p>
+
+<?php
+    include_once '..\view/form.php';
+}
+
+else {
+    $new = new SQL_work;
+    $new->Insert($_POST['Name'], $_POST['ingredients'], $_POST['cook']); ?>
+
+<p>Рецепт успешно добавлен!</p>
+
+<?php
+    include_once '..\index.php';
+}
