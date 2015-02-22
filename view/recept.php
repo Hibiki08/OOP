c75eb2d2805f3e1dfcffec74d8176a46aa807f4f
Hibiki08 <devilcatt1@gmail.com>
2015-02-22 15:17:30 +0300

    Разметка файла рецепта и прикрепление модели

--- view/recept.php
+++ view/recept.php
@@ -1,7 +1,20 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: Hibiki
- * Date: 22.02.2015
- * Time: 0:52
- */ 
+include_once '..\classes\recept.php';
+include_once '..\model\recept.php';
+?>
+<html>
+    <head>
+        <meta charset="utf-8"/>
+    </head>
+    <body>
+    <?php
+    $once_recept = new Recept;
+    $once_recept->Recept();
+    ?>
+    <h2><?php echo $once_recept->name = $name; ?></h2>
+    <h3>Ингредиенты:</h3>
+    <p><?php echo $once_recept->ingredients = $ingredients; ?></p>
+    <h3>Приготовление:</h3>
+    <p><?php echo $once_recept->cook = $cook; ?></p>
+    </body>
+</html>
